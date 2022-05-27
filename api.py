d6c65e746f9ed5e27f15d034f5d63fe798015cb3
Othuke <ajayeor2kay@gmail.com>
2022-05-27 10:00:45 +0100

    api.py updated

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 # Load our model into memory.
 # Please update this path to reflect your own trained model.
 static_model = load_model(
-    path_to_model='assets/trained-models/load_shortfall_extra_trees_regressor.pkl')
+    path_to_model='assets/trained-models/load_shortfall_random forest_regressor.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
